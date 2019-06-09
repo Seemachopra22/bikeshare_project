79461e6b35b66742b73d9a4ce9878f827c653c5b
Seema Jagota <seeja2003@gmail.com>
2019-06-08 22:17:56 -0700

    Added print() statement on line 4

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-
+print("provide information about CITY_DATA, months and days!")
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
